f4ec7f5f07abd7dd94b067d56a3396177e1c8862
GitHub-Kiran <--add>
2021-03-22 07:39:26 +0530

    addec requirements.txt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 import json
 import math
 # import polling
+# from waitress import serve
 
 
 app = Flask(__name__)
@@ -85,7 +86,4 @@ if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
     # app.run(host='0.0.0.0', port=5000, debug=True)
     # app.run(host="0.0.0.0", port="33", debug=True)
-
-
-
 
